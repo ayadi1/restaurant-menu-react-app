da37b342c438cd20e4195ed3deea69db769615b1
ayadi1 <ayadiossama44@gmail.com>
2022-12-05 14:03:40 +0100

    feat: make header responsive

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import './index.css';
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
+import React from "react";
+import "./index.css";
+import App from "./App";
+import { createRoot } from "react-dom/client";
+const root = createRoot(document.getElementById("root")); // createRoot(container!) if you use TypeScript
+root.render(<App />);
