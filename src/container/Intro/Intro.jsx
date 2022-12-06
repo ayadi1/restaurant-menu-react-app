639eb9e022133c8e038303dc0c9548da1cee7272
ayadi1 <ayadiossama44@gmail.com>
2022-12-06 20:49:47 +0100

    add Intro section

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
-
-import './Intro.css';
+import React from "react";
+import { meal } from "../../constants";
+import "./Intro.css";
 
 const Intro = () => (
-  <div>
-    Intro
+  <div className="app__Intro">
+    <video controls autostart autoPlay src={meal} type="video/mp4">
+      <button type="button" id="playVideo">
+        +
+      </button>
+    </video>
   </div>
 );
 
