02a2e3fa49e8547fb7b85bbb2b059e066d3fd3ed
ayadi1 <ayadiossama44@gmail.com>
2022-12-07 10:05:02 +0100

    add FindUs section

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
-
+import React from "react";
+import { images } from "../../constants";
+import './findUs.css';
 const FindUs = () => (
-  <div>
-    FindUs
+  <div
+    className="app__FindUs"
+    style={{ backgroundImage: "url(" + images.bg + ")" }}
+    >
+    <div className="app__FindUs-content__headlines">
+      <h3>Contact</h3>
+      <img src={images.spoon} alt="spoon" />
+      <h2>Find Us</h2>
+      <p>Lane Ends Bungalow, Whatcroft Hall Lane, Rudheath, CW9 7SG</p>
+      <div className="app__FindUs-location">
+        <h4>Opening Hours</h4>
+        <p>Mon - Fri: 10:00 am - 02:00 am</p>
+        <p>Sat - Sun: 10:00 am - 03:00 am</p>
+      </div>
+
+      <button>Explore Menu</button>
+    </div>
+    <div className="app__FindUs-content__image">
+      <img src={images.findus} alt="welcome" />
+    </div>
   </div>
 );
 
